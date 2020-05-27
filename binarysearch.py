79b60a9d40f4e1e77f41b01b0d309d689036e740
Takaki Oshima <takakioshima@gmail.com>
2020-05-27 19:24:29 +0100

    Add binary search

---
+++ binarysearch.py
@@ -0,0 +1,13 @@
+
+lo, hi = 0, 1000
+f = lambda x: x**2
+target = 200
+
+while lo < hi:
+    mid = lo + hi >> 1
+    if f(mid) < target:
+        lo = mid + 1
+    else:
+        hi = mid
+
+print(lo)
